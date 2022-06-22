2d67e9d05f6a2c9cdf2f431a6401ce381c25628c
levimann24 <65826749+levimann24@users.noreply.github.com>
2022-06-22 08:50:54 -0400

    first commit

---
+++ src/GameSettings.cs
@@ -0,0 +1,7 @@
+public static class GameSettings
+{
+    public static int minGameNumber = 1;
+    public static int maxGameNumber = 80;
+    public static int lowestNumberOfPicks = 2;
+    public static int highestNumberOfPicks = 10;
+}
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // List of all stats for each game configuration.
+        List<GameStatistics> gameStatsList = new List<GameStatistics>();
+        int numberOfGamesPerPick = 100000;
+
+        GameStatistics.DisplayStatHeader();
+
+        #region Main Loop -----------------------------------------------------
+        for (int i = GameSettings.lowestNumberOfPicks; i <= GameSettings.highestNumberOfPicks; i++)
+        {
+            var newStats = new GameStatistics(i);
+            for (int j = 0; j < numberOfGamesPerPick; j++)
+            {
+                var newGamePlayerPicks = new PlayerPicks(i);
+                var newGame = new Game(newGamePlayerPicks);
+                newGame.PlayGame();
+                newStats.AddGameResults(newGame);
+                newStats.DisplaySingleRowStats();
+            }
+            Console.WriteLine();
+            gameStatsList.Add(newStats);
+        }
+        #endregion
+
+        Console.WriteLine();
+        Console.WriteLine("Press Enter to close..... ");
+        Console.Read();
+
+    }
+}
